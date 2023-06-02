ffb758ac48648d714492f7e9cc1335ce85137515
Hyungseob Won <stepanowon@hotmail.com>
2023-06-02 14:35:15 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const os = require('os');
+// 
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// 
+const app = express();
+app.get('/', (req, res) => {
+  console.log("## GET /");
+  return res.status(200).send(`
+  <div>
+    <h2> Version: 1.0.0 </h2>
+    <h2> 호스트명 : ${os.hostname()} </h2>
+  </div>
+  `);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
